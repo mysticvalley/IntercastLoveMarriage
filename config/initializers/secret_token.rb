e57a2fdbe2e25f51c9e69ae451bf173c9f758da6
Rajan Maharjan <thamelstreet@gmail.com>
2012-03-01 16:57:54 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntercastLoveMarriage::Application.config.secret_token = '739d1db296b443efb2fc7095c22540eebaaba04551fa15d7a2ea196fb1d7cdf0ed417854611b6e556c0e272f2392f8fd2da18b1761c788a7fb24335d31665327'
